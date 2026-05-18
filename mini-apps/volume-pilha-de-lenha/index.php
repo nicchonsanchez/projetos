e310a78b4492cf729c96d6201fd9ff6b7a3415b3
creatyze <creatyzestudios@gmail.com>
2026-05-17 23:22:03 -0300

    fix(calculos): botao Voltar dos 3 calcs aponta pra ../calculos-florestais/
    Antes: href=../ levava pra raiz do app.nicchon.com (perdia o contexto hub)
    Agora: href=../calculos-florestais/ + texto 'Voltar aos calculos'
    
    Header back-link continua apontando pra raiz (fuga total) - hierarquia
    clara com 2 paths: contexto vs raiz.

--- mini-apps/volume-pilha-de-lenha/index.php
+++ mini-apps/volume-pilha-de-lenha/index.php
@@ -103,10 +103,10 @@
     </div>
   </div>
 
-  <a href="../">
+  <a href="../calculos-florestais/">
     <div class="button voltar">
       <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 14 4 9l5-5"/><path d="M4 9h10.5a5.5 5.5 0 0 1 5.5 5.5v0a5.5 5.5 0 0 1-5.5 5.5H11"/></svg>
-      Voltar
+      Voltar aos cálculos
     </div>
   </a>
 </section>
